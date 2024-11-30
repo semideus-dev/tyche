5a83e9e0ac971815679bc584e8f150e8a760077b
Semideus <semideus.dev@gmail.com>
2024-11-30 23:58:45 +0530

    v0.2.1 - api public routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
-  "/api/webhooks(.*)",
+  "/api(.*)",
   "/sign-in(.*)",
   "/sign-up(.*)",
 ]);
