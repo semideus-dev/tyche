48d320461bb6fc28fa22c7d9e79565daa8cf6687
Semideus <semideus.dev@gmail.com>
2024-11-30 21:42:38 +0530

    v0.1.1 - user button

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { LogIn } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Navbar() {
   const router = useRouter();
@@ -14,7 +16,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed z-50 flex w-screen items-center justify-between p-1 px-5 md:px-10 backdrop-blur-md">
+    <nav className="fixed z-50 flex w-screen items-center justify-between p-1 px-5 backdrop-blur-md md:px-10">
       <div className="flex items-center gap-2">
         <Link href={"/"}>
           <Image
@@ -27,7 +29,7 @@ export default function Navbar() {
         </Link>
       </div>
       <div className="flex items-center gap-8 capitalize">
-        <div className="hidden md:flex gap-8">
+        <div className="hidden gap-8 md:flex">
           <Link href={"#"}>
             <span className="underline-offset-4 hover:text-primary hover:underline">
               About
@@ -39,10 +41,18 @@ export default function Navbar() {
             </span>
           </Link>
         </div>
-        <Button variant="muted" onClick={() => handleSignIn()}>
-          <span>Sign in</span>
-          <LogIn className="h-5 w-5" />
-        </Button>
+        <SignedOut>
+          <Button variant="muted" onClick={() => handleSignIn()}>
+            <span>Sign in</span>
+            <LogIn className="h-5 w-5" />
+          </Button>
+        </SignedOut>
+        <SignedIn>
+          <UserButton
+            appearance={{ baseTheme: dark }}
+            userProfileProps={{ appearance: { baseTheme: dark } }}
+          />
+        </SignedIn>
       </div>
     </nav>
   );
